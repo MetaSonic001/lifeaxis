b136531b5da3247af4702f450f6bf692f64406af
Shaun Mendes <shaunkmmendes@gmail.com>
2025-04-18 23:20:47 +0530

    c

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,49 +1,34 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs';
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "LifeAxis",
   description: "Created by LifeAxis Team",
 };
 
-export default function RootLayout({
+export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      {/* <html lang="en"> */}
-      {/* <body
+    <html lang="en">
+      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > */}
-      {/* <SignedOut>
-          <SignInButton />
-        </SignedOut> */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      {children}
-      {/* </body> */}
-      {/* </html> */}
-    </ClerkProvider>
+      >
+        {children}
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-'use client'
+// app/page.tsx
+import { redirect } from 'next/navigation';
 
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-
-const HomeRedirect = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Redirect to '/home' when the component mounts
-    router.push('/home')
-  }, [router])
-
-  return null // You don't need to render anything on the main page
+export default function Page() {
+  redirect('/home');
 }
-
-export default HomeRedirect
